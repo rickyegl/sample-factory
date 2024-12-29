c696a8a7ddf4f837f13729463e97b275bacde0b0
rickyegl <135090025+rickyegl@users.noreply.github.com>
2024-12-28 20:31:59 -0600

    Update atari_utils.py

--- sf_examples/atari/atari_utils.py
+++ sf_examples/atari/atari_utils.py
@@ -80,7 +80,7 @@ ATARI_ENVS = [
     AtariSpec("atari_wizardofwor", "WizardOfWorNoFrameskip-v4"),
     AtariSpec("atari_yarsrevenge", "YarsRevengeNoFrameskip-v4"),
     AtariSpec("atari_zaxxon", "ZaxxonNoFrameskip-v4"),
-    AtariSpec("mario", "SuperMarioBros-v0"),
+    AtariSpec("mario", "gym_super_mario_bros:SuperMarioBros-v0"),
     AtariSpec("marior", "SuperMarioBrosRandomStages-v0"),
 ]
 
