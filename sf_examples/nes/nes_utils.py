d7de2af895b73d2757033f3f73845e3eaa2fe6b5
rickyegl <135090025+rickyegl@users.noreply.github.com>
2024-12-29 18:09:25 +0000

    Fix FrameStack wrapper usage in NES environment setup

--- sf_examples/nes/nes_utils.py
+++ sf_examples/nes/nes_utils.py
@@ -54,6 +54,6 @@ def make_nes_env(env_name, cfg, env_config, render_mode: Optional[str] = None):
     env = ClipRewardEnv(env)
     env = gym.wrappers.ResizeObservation(env, (84, 84))
     #env = gym.wrappers.GrayScaleObservation(env)
-    env = gym.wrappers.FrameStackObservation(env, cfg.env_framestack)
+    env = gym.wrappers.FrameStack(env, cfg.env_framestack)
     env = NumpyObsWrapper(env)
     return env
